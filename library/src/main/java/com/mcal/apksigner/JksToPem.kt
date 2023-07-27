465f142afbe829602412399fa4f63814a48ce6f2
Иван Тимашков <timscriptov@mail.ru>
2023-07-27 14:40:27 +0500

    Fix jks to pk8 + pem converter

--- library/src/main/java/com/mcal/apksigner/JksToPem.kt
+++ library/src/main/java/com/mcal/apksigner/JksToPem.kt
@@ -1,10 +1,12 @@
 package com.mcal.apksigner
 
+import com.mcal.apksigner.utils.Base64
 import com.mcal.apksigner.utils.KeyStoreFileManager
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.security.PrivateKey
+import java.security.cert.X509Certificate
 
 object JksToPem {
     @JvmStatic
@@ -16,15 +18,23 @@ object JksToPem {
 
             val alias = keyStore.aliases().nextElement()
 
-            FileOutputStream(pk8File).apply {
-                write((keyStore.getKey(alias, jksPassword.toCharArray()) as PrivateKey).encoded)
-            }.close()
+            FileOutputStream(pk8File).use { pk8OutputStream ->
+                pk8OutputStream.write(
+                    (keyStore.getKey(
+                        alias,
+                        jksPassword.toCharArray()
+                    ) as PrivateKey).encoded
+                )
+            }
 
-            FileOutputStream(pemFile).apply {
-                write("-----BEGIN CERTIFICATE-----\n".toByteArray())
-                write(keyStore.getCertificate(alias).encoded)
-                write("\n-----END CERTIFICATE-----\n".toByteArray())
-            }.close()
+            FileOutputStream(pemFile).use { pemOutputStream ->
+                pemOutputStream.write("-----BEGIN CERTIFICATE-----\n".toByteArray())
+                pemOutputStream.write(
+                    Base64.encode((keyStore.getCertificate(alias) as X509Certificate).encoded)
+                        .replace("(.{64})".toRegex(), "$1\n").toByteArray()
+                )
+                pemOutputStream.write("\n-----END CERTIFICATE-----\n".toByteArray())
+            }
             true
         } catch (e: Exception) {
             e.printStackTrace()
